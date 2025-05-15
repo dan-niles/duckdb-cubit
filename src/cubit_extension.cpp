51cf7595970a8fd5ad390ac8ddb08aa996a4f6d3
dan-niles <niles_dan@live.com>
2025-05-15 12:21:09 +0530

    Rename quack to cubit

--- src/cubit_extension.cpp
+++ src/cubit_extension.cpp
@@ -1,6 +1,6 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "quack_extension.hpp"
+#include "cubit_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
@@ -13,21 +13,21 @@
 
 namespace duckdb {
 
-inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+inline void CubitScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
     auto &name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(
 	    name_vector, result, args.size(),
 	    [&](string_t name) {
-			return StringVector::AddString(result, "Quack "+name.GetString()+" 🐥");
+			return StringVector::AddString(result, "Cubit "+name.GetString()+" 🐥");
         });
 }
 
-inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+inline void CubitOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
     auto &name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(
 	    name_vector, result, args.size(),
 	    [&](string_t name) {
-			return StringVector::AddString(result, "Quack " + name.GetString() +
+			return StringVector::AddString(result, "Cubit " + name.GetString() +
                                                      ", my linked OpenSSL version is " +
                                                      OPENSSL_VERSION_TEXT );
         });
@@ -35,25 +35,25 @@ inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state
 
 static void LoadInternal(DatabaseInstance &instance) {
     // Register a scalar function
-    auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
-    ExtensionUtil::RegisterFunction(instance, quack_scalar_function);
+    auto cubit_scalar_function = ScalarFunction("cubit", {LogicalType::VARCHAR}, LogicalType::VARCHAR, CubitScalarFun);
+    ExtensionUtil::RegisterFunction(instance, cubit_scalar_function);
 
     // Register another scalar function
-    auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, quack_openssl_version_scalar_function);
+    auto cubit_openssl_version_scalar_function = ScalarFunction("cubit_openssl_version", {LogicalType::VARCHAR},
+                                                LogicalType::VARCHAR, CubitOpenSSLVersionScalarFun);
+    ExtensionUtil::RegisterFunction(instance, cubit_openssl_version_scalar_function);
 }
 
-void QuackExtension::Load(DuckDB &db) {
+void CubitExtension::Load(DuckDB &db) {
 	LoadInternal(*db.instance);
 }
-std::string QuackExtension::Name() {
-	return "quack";
+std::string CubitExtension::Name() {
+	return "cubit";
 }
 
-std::string QuackExtension::Version() const {
-#ifdef EXT_VERSION_QUACK
-	return EXT_VERSION_QUACK;
+std::string CubitExtension::Version() const {
+#ifdef EXT_VERSION_CUBIT
+	return EXT_VERSION_CUBIT;
 #else
 	return "";
 #endif
@@ -63,12 +63,12 @@ std::string QuackExtension::Version() const {
 
 extern "C" {
 
-DUCKDB_EXTENSION_API void quack_init(duckdb::DatabaseInstance &db) {
+DUCKDB_EXTENSION_API void cubit_init(duckdb::DatabaseInstance &db) {
     duckdb::DuckDB db_wrapper(db);
-    db_wrapper.LoadExtension<duckdb::QuackExtension>();
+    db_wrapper.LoadExtension<duckdb::CubitExtension>();
 }
 
-DUCKDB_EXTENSION_API const char *quack_version() {
+DUCKDB_EXTENSION_API const char *cubit_version() {
 	return duckdb::DuckDB::LibraryVersion();
 }
 }
